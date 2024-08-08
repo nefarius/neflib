12b2c1908b874ce8cac89e477f2527b470129404
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-08-09 01:14:58 +0200

    Migrated DeleteDriverService

--- include/nefarius/neflib/MiscWinApi.hpp
+++ include/nefarius/neflib/MiscWinApi.hpp
@@ -10,4 +10,11 @@ namespace nefarius::winapi
 	std::expected<bool, nefarius::utilities::Win32Error> IsAppRunningAsAdminMode();
 
 	std::expected<void, nefarius::utilities::Win32Error> AdjustProcessPrivileges();
+
+	namespace services
+	{
+		std::expected<void, nefarius::utilities::Win32Error> CreateDriverService(PCSTR ServiceName, PCSTR DisplayName, PCSTR BinaryPath);
+
+		std::expected<void, nefarius::utilities::Win32Error> DeleteDriverService(PCSTR ServiceName);
+	}
 }
--- src/MiscWinApi.cpp
+++ src/MiscWinApi.cpp
@@ -118,3 +118,81 @@ std::expected<void, nefarius::utilities::Win32Error> nefarius::winapi::AdjustPro
 
 	return {};
 }
+
+std::expected<void, nefarius::utilities::Win32Error> nefarius::winapi::services::CreateDriverService(PCSTR ServiceName,
+	PCSTR DisplayName, PCSTR BinaryPath)
+{
+	SC_HANDLE hSCManager = OpenSCManagerA(
+		nullptr,
+		nullptr,
+		SC_MANAGER_CREATE_SERVICE
+	);
+
+	if (!hSCManager)
+	{
+		return std::unexpected(utilities::Win32Error("OpenSCManagerA"));
+	}
+
+	SCOPE_GUARD_CAPTURE({ CloseServiceHandle(hSCManager); }, hSCManager);
+
+	SC_HANDLE hService = CreateServiceA(
+		hSCManager,
+		ServiceName,
+		DisplayName,
+		SERVICE_START | DELETE | SERVICE_STOP,
+		SERVICE_KERNEL_DRIVER,
+		SERVICE_DEMAND_START,
+		SERVICE_ERROR_IGNORE,
+		BinaryPath,
+		nullptr,
+		nullptr,
+		nullptr,
+		nullptr,
+		nullptr
+	);
+
+	if (!hService)
+	{
+		return std::unexpected(utilities::Win32Error("CreateServiceA"));
+	}
+
+	SCOPE_GUARD_CAPTURE({ CloseServiceHandle(hService); }, hService);
+
+	return {};
+}
+
+std::expected<void, nefarius::utilities::Win32Error> nefarius::winapi::services::DeleteDriverService(PCSTR ServiceName)
+{
+	SC_HANDLE hSCManager = OpenSCManagerA(
+		nullptr,
+		nullptr,
+		SC_MANAGER_CREATE_SERVICE
+	);
+
+	if (!hSCManager)
+	{
+		return std::unexpected(utilities::Win32Error("OpenSCManagerA"));
+	}
+
+	SCOPE_GUARD_CAPTURE({ CloseServiceHandle(hSCManager); }, hSCManager);
+
+	SC_HANDLE hService = OpenServiceA(
+		hSCManager,
+		ServiceName,
+		SERVICE_START | DELETE | SERVICE_STOP
+	);
+
+	if (!hService)
+	{
+		return std::unexpected(utilities::Win32Error("OpenServiceA"));
+	}
+
+	SCOPE_GUARD_CAPTURE({ CloseServiceHandle(hService); }, hService);
+
+	if (!DeleteService(hService))
+	{
+		return std::unexpected(utilities::Win32Error("DeleteService"));
+	}
+
+	return {};
+}
