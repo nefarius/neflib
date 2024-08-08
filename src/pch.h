92551440e851467a3451363393fdc05093bb1823
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-08-08 19:50:13 +0200

    Added MultiStringArray and Win32Error

--- src/pch.h
+++ src/pch.h
@@ -14,14 +14,16 @@
 // 
 #include <string>
 #include <type_traits>
+#include <vector>
+#include <format>
 
 //
 // Public headers
 // 
 #include <nefarius/neflib/UniUtil.hpp>
-
 #include <nefarius/neflib/HDEVINFOHandleGuard.hpp>
 #include <nefarius/neflib/HKEYHandleGuard.hpp>
 #include <nefarius/neflib/INFHandleGuard.hpp>
-
 #include <nefarius/neflib/LibraryHelper.hpp>
+#include <nefarius/neflib/MultiStringArray.hpp>
+#include <nefarius/neflib/Win32Error.hpp>
