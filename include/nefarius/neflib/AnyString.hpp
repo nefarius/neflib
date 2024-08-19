5e8ed92446a5156fb869d875c4bd7dbab6b92bd6
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-08-19 23:01:18 +0200

    Update AnyString.hpp

--- include/nefarius/neflib/AnyString.hpp
+++ include/nefarius/neflib/AnyString.hpp
@@ -2,8 +2,8 @@
 
 namespace nefarius::utilities
 {
-	template <class T>
-	concept string_type = std::derived_from<T, std::basic_string<typename T::value_type, typename T::allocator_type>>;
+	template<class T>
+concept string_type = std::same_as<T, std::basic_string<typename T::value_type, typename T::traits_type, typename T::allocator_type>>;
 
 	template <typename StringType>
 	void StripNullCharacters(StringType& s);
